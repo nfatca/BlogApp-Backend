76b1af0b898c19298c483dd9d284f07b7184bace
Nurullah Furkan Atca <nfurkanatca@gmail.com>
2023-03-06 17:36:24 +0300

    feat: like create

--- blog/serializers.py
+++ blog/serializers.py
@@ -4,43 +4,49 @@ from .models import Post,Like,Comment,PostView
 import datetime
 
 class CommentSerializer(serializers.ModelSerializer):
-    
+
     post = serializers.StringRelatedField()
-    post_id = serializers.IntegerField()
+    post_id = serializers.IntegerField(read_only=True)
     commentor = serializers.StringRelatedField()
-    commentor_id = serializers.IntegerField()
+    commentor_id = serializers.IntegerField(read_only=True)
     created_date = serializers.SerializerMethodField()
-    
-    
+
+
     class Meta:
         model = Comment
         fields = ('id', 'post_id', 'post', 'title', 'comment', 'commentor','commentor_id', 'created_date')
-        
-    def get_created_date(self,obj):
-        return datetime.datetime.strftime(obj.created_date, '%d.%m.%Y')
+
+
+    def get_created_date(self, obj):
+        return datetime.datetime.strftime(obj.created_date, '%d,%m,%Y')
     
+
+
 class LikeSerializer(serializers.ModelSerializer):
 
     post = serializers.StringRelatedField()
-    post_id = serializers.IntegerField()
+    post_id = serializers.IntegerField(read_only=True)
     liker = serializers.StringRelatedField()
-    liker_id = serializers.IntegerField()
+    liker_id = serializers.IntegerField(read_only=True)
 
     class Meta:
         model = Like
         fields = ('id', 'post', 'post_id','liker', 'liker_id', 'is_liked')
 
+
+
 class PostSerializer(serializers.ModelSerializer):
-    
+
     author = serializers.StringRelatedField()
     author_id = serializers.IntegerField(read_only=True)
-    like_count = serializers.SerializerMethodField()
-    visit_count = serializers.SerializerMethodField()
     created_date = serializers.SerializerMethodField()
+    visit_count = serializers.SerializerMethodField()
+    like_count = serializers.SerializerMethodField()
+
     comments = CommentSerializer(many=True, read_only=True)
-    
+
     class Meta:
-        model= Post
+        model = Post
         fields = (
             'id',
             'image',
@@ -52,16 +58,16 @@ class PostSerializer(serializers.ModelSerializer):
             'author_id',
             'comments',
             'slug',
+            'visit_count',
             'like_count',
-            'visit_count'
+
         )
-        
-    def get_created_date(self,obj):
-        return datetime.datetime.strftime(obj.created_date, '%d.%m.%Y')
+
+    def get_created_date(self, obj):
+        return datetime.datetime.strftime(obj.created_date, '%d,%m,%Y')
     
-    def get_like_count(self,obj):
-        return Like.objects.filter(post_id = obj.id, is_liked=True).count()
+    def get_like_count(self, obj):
+        return Like.objects.filter(post_id=obj.id, is_liked=True).count()
     
-    def get_visit_count(self,obj):
-        return PostView.objects.filter(post =obj).count()
-    
+    def get_visit_count(self, obj):
+        return PostView.objects.filter(post=obj).count()
--- blog/views.py
+++ blog/views.py
@@ -10,21 +10,38 @@ from rest_framework.response import Response
 class PostMVS(ModelViewSet):
     queryset = Post.objects.filter(is_published=True)
     serializer_class = PostSerializer
-    
+
+
+    def retrieve(self, request, *args, **kwargs):
+        instance = self.get_object()
+        postview = PostView.objects.filter(user=request.user, post=instance)
+        if not postview.exists():
+            PostView.objects.create(post=instance, user=request.user)
+        serializer = self.get_serializer(instance)
+        return Response(serializer.data)
+
+
     def create(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         current_user = self.request.user
+        print(current_user)
         serializer.validated_data['author_id'] = current_user.id
         self.perform_create(serializer)
         headers = self.get_success_headers(serializer.data)
         return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+    
+
+
+
+
+
 
 class CommentMVS(ModelViewSet):
     queryset = Comment.objects.all().select_related('post')
     serializer_class = CommentSerializer
-    
-    
+
+
     def create(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
@@ -32,17 +49,16 @@ class CommentMVS(ModelViewSet):
         post_id = self.kwargs.get('post_pk')
         comment = Comment.objects.filter(post_id=post_id, commentor_id=current_user.id)
         if comment.exists():
-            raise ValidationError(f"{current_user} is commented before")
+            raise ValidationError(f" {current_user} is commented before")
         else:
             post_id = self.kwargs.get('post_pk')
             serializer.validated_data['commentor_id'] = current_user.id
             serializer.validated_data['post_id'] = post_id
-        # serializer.validated_data['author_id'] = current_user.id
         self.perform_create(serializer)
         headers = self.get_success_headers(serializer.data)
         return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
-    
-    
+
+
     def get_queryset(self):
         post_id = self.kwargs.get('post_pk')
         if post_id == None:
@@ -51,9 +67,50 @@ class CommentMVS(ModelViewSet):
             try:
                 post = Post.objects.get(id=post_id)
             except Post.DoesNotExist:
-                raise NotFound("A post with this is does not exist")
-        return self.queryset.filter(post=post)
-    
+                raise NotFound("A post with this id does not exist")
+        return self.queryset.filter(post = post)
+
+
 class LikeMVS(ModelViewSet):
     queryset = Like.objects.all()
     serializer_class = LikeSerializer
+    
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        current_user = self.request.user
+        post_id = self.kwargs.get('post_pk')
+        like = Like.objects.filter(post_id=post_id, liker_id=current_user.id)
+        if like.exists():
+            raise Response(serializer.data)
+        else:
+            post_id = self.kwargs.get('post_pk')
+            serializer.validated_data['liker_id'] = current_user.id
+            serializer.validated_data['post_id'] = post_id
+            serializer.validated_data['is_liked'] = True
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+    
+    def update(self, request, *args, **kwargs):
+        partial = kwargs.pop('partial', False)
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data, partial=partial)
+        serializer.is_valid(raise_exception=True)
+        
+        if self.request.user.id == instance.liker.id:
+            if instance.is_liked == True:
+                serializer.validated_date['is_liked'] = False
+            else:
+                serializer.validated_date['is_liked'] = True
+                
+        
+        
+        self.perform_update(serializer)
+
+        if getattr(instance, '_prefetched_objects_cache', None):
+            # If 'prefetch_related' has been applied to a queryset, we need to
+            # forcibly invalidate the prefetch cache on the instance.
+            instance._prefetched_objects_cache = {}
+
+        return Response(serializer.data)
